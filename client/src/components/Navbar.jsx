6fdfec2f2d9d65f134b94e72e3f4fecdd3db3983
Samson Agbo <agbosamsonite@gmail.com>
2023-11-01 11:34:45 +0100

    finishing touches before corrections

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           <NavBarItem key={item + index} title={item} />
         ))}
         <li className="bg-[#5c9cd6] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#151725]">
-          Connect
+          Login
         </li>
       </ul>
       <div className="flex relative">
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
+import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
+import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from "./";
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -19,12 +21,16 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 
 const Welcome = () => {
 
-    const connectWallet = () => {
+    const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext);
 
-    }
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
 
-    const handleSubmit = () => {
+        e.preventDefault();
 
+        if(!addressTo || !amount || !keyword || !message) return;
+
+        sendTransaction();
     }
 
     return (
@@ -37,14 +43,18 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the Crypto World. Send Crypto Annonymously on 9th DEX
                     </p>
-                    <button
-                    type="button"
-                    onClick={connectWallet}
-                    className="flex flex-row justify-center items-center my-5 bg-[#5c9cd6] p-3 rounded-full cursor-pointer hover:bg-[#2c2f4f]"
-                    >
-                        <p className="text-white text-base font-semibold">Connect Wallet</p>
-                    </button>
-
+                    {!currentAccount && (
+                        <button
+                            type="button"
+                            onClick={connectWallet}
+                            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+                        >
+                            <AiFillPlayCircle className="text-white mr-2" />
+                            <p className="text-white text-base font-semibold">
+                                Connect Wallet
+                            </p>
+                        </button>
+                    )}
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
                             Reliability
@@ -84,10 +94,10 @@ const Welcome = () => {
                     </div>
 
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
-                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
-                        <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={() => {}} />
-                        <Input placeholder="Enter Anonymous Message" name="message" type="text" handleChange={() => {}} />
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+                        <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={handleChange} />
+                        <Input placeholder="Enter Anonymous Message" name="message" type="text" handleChange={handleChange} />
 
                         <div className="h-[1px] w-full bg-gray-400 my-2" />
 
@@ -109,5 +119,4 @@ const Welcome = () => {
         </div>
     );
 }
-
 export default Welcome;
